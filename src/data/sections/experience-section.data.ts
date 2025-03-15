17b63734edf5d7c1f89c69af7b1a97cf1aaec696
VISHAL KUMAR <vishaltalreja01@gmail.com>
2025-03-15 13:57:04 +0500

    updated the experience section

--- src/data/sections/experience-section.data.ts
+++ src/data/sections/experience-section.data.ts
@@ -1,18 +1,23 @@
 import type { ExperienceSection } from '@/types/sections/experience-section.types';
 import type { ReadonlyDeep } from 'type-fest';
-import { facebook, github, instagram, linkedin, twitter, website } from '../helpers/links';
+import { linkedin, website } from '../helpers/links';
+// import { facebook, github, instagram, linkedin, twitter, website } from '../helpers/links';
 import {
-  chakraUi,
-  eslint,
-  firebase,
   nextJs,
-  nx,
-  pnpm,
   react,
+  html,
+  css,
+  php,
+  javascript,
+  mysql,
   reactQuery,
   tailwindCss,
-  typescript,
-  vue,
+  // typescript,
+  oracle,
+  oracleApex,
+  sql,
+  strapi,
+  materialUI,
 } from '../helpers/skills';
 
 const experienceSectionData = {
@@ -24,59 +29,74 @@ const experienceSectionData = {
   },
   jobs: [
     {
-      role: 'Senior front-end developer',
-      company: 'Google',
-      image: import('@/assets/logos/google-logo.jpg'),
-      dates: [new Date('2020-02'), null],
+      role: 'Oracle Apex Developer',
+      company: 'Pakistan State Oil (PSO)',
+      image: import('@/assets/logos/pso-logo.png'),
+      dates: [new Date('2025-02'), null],
       description: `
-        - In tristique vulputate augue vel egestas.
-        - Quisque ac imperdiet tortor, at lacinia ex.
-        - Duis vel ex hendrerit, commodo odio sed, aliquam enim.
-        - Ut arcu nulla, tincidunt eget arcu eget, molestie vulputate nisi.
-        - Nunc malesuada leo et est iaculis facilisis.
-        - Fusce eu urna ut magna malesuada fringilla.
+        - Building and maintaining dynamic web applications using Oracle Apex to meet requirements.
       `,
       tagsList: {
         title: 'Technologies',
-        tags: [react(), nextJs(), typescript(), nx(), firebase()],
+        tags: [oracleApex(), sql(), mysql(), oracle()],
       },
-      links: [facebook({ url: '#' }), linkedin({ url: '#' })],
+      links: [
+        website({ url: 'https://psopk.com/' }),
+        linkedin({ url: 'https://www.linkedin.com/company/psopakistan' }),
+      ],
     },
     {
       role: 'React.js developer',
-      company: 'Facebook',
-      image: import('@/assets/logos/facebook-logo.png'),
-      dates: [new Date('2019-04'), new Date('2020-02')],
+      company: 'Kava Up LLC',
+      image: import('@/assets/logos/kavaup-logo.jpg'),
+      dates: [new Date('2023-12'), null],
       description: `
-        - Aenean eget ultricies felis. Pellentesque dictum massa ut tellus eleifend, sed posuere massa mattis.
-        - Ut posuere massa lacus, eleifend molestie tortor auctor vel.
-        - Sed sed sollicitudin eros, id ultricies mi. Aliquam sodales elit vel ante tempor, non vehicula nibh facilisis.
-        - Cras feugiat ultricies maximus. Aliquam tristique ex odio, ac semper urna accumsan a.
+        - Developed and optimized responsive web apps using React.js, Next.js, Tailwind CSS, and Material UI, achieving a 70% boost in user engagement.
+        - Built dynamic forms using Formik and implemented animations with React Motion for enhanced user experiences.
+        - Integrated different 3rd party services such as Chatling ai Bot, Strapi, Google captcha, iconify-icon, Google fonts to enhance application features and user interaction.
+        - Proactively liaised with the desgin team and project manager to ensure efficient and responsive.
       `,
       tagsList: {
         title: 'Technologies',
-        tags: [react(), reactQuery(), chakraUi(), eslint()],
+        tags: [react(), nextJs(), tailwindCss(), materialUI(), reactQuery(), strapi()],
       },
-      links: [website({ url: '#' }), instagram({ url: '#' })],
+      links: [
+        website({ url: 'https://www.kavaup.io' }),
+        linkedin({ url: 'https://www.linkedin.com/company/kava-up-llc' }),
+      ],
     },
     {
-      role: 'Junior front-end developer',
-      company: 'GitLab',
-      image: import('@/assets/logos/gitlab-logo.png'),
-      dates: [new Date('2016-09'), new Date('2019-04')],
+      role: 'Web Developer Lead',
+      company: 'RKP Center',
+      image: import('@/assets/logos/rkp-logo.png'),
+      dates: [new Date('2023-01'), new Date('2023-11')],
       description: `
-        Nulla volutpat justo ante, rhoncus posuere massa egestas in:
-
-        - Quisque pellentesque, dolor nec sollicitudin iaculis, sem velit consequat ligula, eget tempus ligula leo et est.
-        - Maecenas ut elit sit amet nibh maximus condimentum in nec lorem. Pellentesque tincidunt odio vel leo suscipit, in interdum mi gravida.
-
-        Donec non vulputate augue 🤓
+        - Developed dynamic web applications using PHP, Laravel, and React, delivering reliable and scalable client solutions.
+        - Managed full project lifecycles for 10+ projects, meeting deadlines and achieving 100% client satisfaction.
       `,
       tagsList: {
         title: 'Technologies',
-        tags: [vue(), tailwindCss(), pnpm()],
+        tags: [php(), html(), css(), mysql(), react(), tailwindCss()],
       },
-      links: [twitter({ url: '#' }), github({ url: '#' })],
+      links: [
+        website({ url: 'https://rkp.center/' }),
+        linkedin({ url: 'https://www.linkedin.com/company/rkpcenter/' }),
+      ],
+    },
+    {
+      role: 'Frontend Developer',
+      company: 'Fiverr',
+      image: import('@/assets/logos/fiverr-logo.jpg'),
+      dates: [new Date('2022-04'), new Date('2023-02')],
+      description: `
+        - Delivered 40+ projects focused on e-commerce and open-source customization using React.js, HTML, CSS, JS, PHP, MySQL, TailwindCSS.
+        - Helped to grow 10+ Startups of international clients which includes RootGuard, UHQ Logs, Innovatex Gaming, ADS Center, Kurate.
+      `,
+      tagsList: {
+        title: 'Technologies',
+        tags: [react(), tailwindCss(), html(), css(), javascript(), php(), mysql()],
+      },
+      links: [website({ url: 'https://fiverr.com' }), linkedin({ url: 'https://www.linkedin.com/company/fiverr-com' })],
     },
   ],
 } as const satisfies ReadonlyDeep<ExperienceSection>;
